381ef56132c861e63f7e3d276281b0f3621e39bf
Rafael Barbolo <rafael.barbolo@infosimples.com.br>
2020-04-07 13:46:24 -0300

    Finish M07

--- lib/graders/m07/exemplos-aula/deadlock.2.rb
+++
@@ -1,5 +0,0 @@
-queue = Queue.new # an empty queue with no jobs
-Thread.new do
-  job = queue.pop # it’ll never pop a job
-  puts 'Job is finished'
-end.join
--- lib/graders/m07/exemplos-aula/processos.8.rb
+++
@@ -1,7 +0,0 @@
-puts "Hi, I'm the parent. Process ID: #{Process.pid}"
-
-fork do
-  puts "I'm the child. Process ID: #{Process.pid}"
-end
-
-puts "Parent here! I've skipped fork (#{Process.pid})"
